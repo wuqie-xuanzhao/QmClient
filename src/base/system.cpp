b7c35a495b6bd65926a358367962a0f749e91b7c
Robert Müller <robytemueller@gmail.com>
2026-01-04 22:29:41 +0100

    Replace `secure_rand` with `secure_random_fill`/`secure_rand_below`
    The `secure_rand` function returned an integer below `RAND_MAX` (which can be as low as `0x7FFF`) to be compatible with `rand()`, but it was expected to produce larger values for random filenames and ports.
    
    (cherry picked from commit b981ad7749fe5e8b871be72d16a9839e305c1fd6)

--- src/base/system.cpp
+++ src/base/system.cpp
@@ -2580,7 +2580,8 @@ int fs_remove(const char *filename)
 	}
 	const std::wstring wide_filename = windows_utf8_to_wide(filename);
 
-	unsigned random_num = secure_rand();
+	unsigned random_num;
+	secure_random_fill(&random_num, sizeof(random_num));
 	std::wstring wide_filename_temp;
 	do
 	{
@@ -3203,13 +3204,6 @@ void secure_random_fill(void *bytes, unsigned length)
 #else
 	dbg_assert(length == io_read(secure_random_data.urandom, bytes, length), "io_read returned with a short read");
 #endif
-}
-
-int secure_rand()
-{
-	unsigned int i;
-	secure_random_fill(&i, sizeof(i));
-	return (int)(i % RAND_MAX);
 }
 
 // From https://graphics.stanford.edu/~seander/bithacks.html#RoundUpPowerOf2.
