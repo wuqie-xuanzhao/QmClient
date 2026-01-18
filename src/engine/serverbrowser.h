4db8ba50fb58206e476459b80a7fcf3606f7847f
Robert Müller <robytemueller@gmail.com>
2026-01-18 16:38:17 +0100

    Render current number of players in community filter

--- src/engine/serverbrowser.h
+++ src/engine/serverbrowser.h
@@ -217,6 +217,7 @@ class CCommunity
 	char m_aIconUrl[128];
 	std::vector<CCommunityCountry> m_vCountries;
 	std::vector<CCommunityType> m_vTypes;
+	int m_NumPlayers = 0;
 	bool m_HasFinishes = false;
 	std::unordered_set<CCommunityMap, CCommunityMap::SHash> m_FinishedMaps;
 
@@ -239,6 +240,7 @@ public:
 	bool HasType(const char *pTypeName) const;
 	bool HasRanks() const { return m_HasFinishes; }
 	CServerInfo::ERankState HasRank(const char *pMap) const;
+	int NumPlayers() const { return m_NumPlayers; }
 };
 
 class IFilterList
