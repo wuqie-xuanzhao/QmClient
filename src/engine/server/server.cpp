7acb1417309f9d48258ef31bc88aa9083d43ee35
heinrich5991 <heinrich5991@gmail.com>
2026-03-10 17:02:14 +0100

    Remove stray `CSnapshotDelta::SetStaticsize` calls

--- src/engine/server/server.cpp
+++ src/engine/server/server.cpp
@@ -1107,8 +1107,6 @@ void CServer::DoSnapshot()
 			}
 
 			// create delta
-			m_SnapshotDelta.SetStaticsize(protocol7::NETEVENTTYPE_SOUNDWORLD, m_aClients[i].m_Sixup);
-			m_SnapshotDelta.SetStaticsize(protocol7::NETEVENTTYPE_DAMAGE, m_aClients[i].m_Sixup);
 			char aDeltaData[CSnapshot::MAX_SIZE];
 			int DeltaSize = m_SnapshotDelta.CreateDelta(pDeltashot, pData, aDeltaData);
 
