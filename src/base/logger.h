7d9110a2e3dcdbcc4c5546c5f030e2b7cca1a5a5
Robert Müller <robytemueller@gmail.com>
2026-04-15 21:26:15 +0200

    Document that `log_logger_stdout` function can return `nullptr`
    (cherry picked from commit 8610eafef8044dca7db238e2e4dc3f8ee613c3a2)

--- src/base/logger.h
+++ src/base/logger.h
@@ -217,6 +217,8 @@ std::unique_ptr<ILogger> log_logger_prefix_file(IOHANDLE file, const char *syste
  * @ingroup Log
  *
  * Logger for writing logs to the standard output (stdout).
+ *
+ * @remark This function can return `nullptr` if the standard output is not available.
  */
 std::unique_ptr<ILogger> log_logger_stdout();
 
