c03b7209f4809d59ca8fed5eb85c9c2efa98db7e
heinrich5991 <heinrich5991@gmail.com>
2026-04-23 17:25:08 +0200

    Use `dbg_assert_failed` on Rust panic
    Fixes #12074.

--- src/engine/shared/engine.cpp
+++ src/engine/shared/engine.cpp
@@ -2,7 +2,12 @@
 /* If you are missing that file, acquire a complete release at teeworlds.com.                */
 
 #include <base/logger.h>
+#include <base/net.h>
+#include <base/os.h>
+#include <base/rust.h>
+#include <base/str.h>
 #include <base/system.h>
+#include <base/time.h>
 
 #include <engine/console.h>
 #include <engine/engine.h>
@@ -65,6 +70,8 @@ namespace
 				{
 					log_info("engine", "operating system version: %s", aVersionStr);
 				}
+
+				rust_panic_use_dbg_assert();
 
 				// init the network
 				net_init();
