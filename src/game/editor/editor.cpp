bfc087ec2b99b8b1e8647c2ce38396bfb27a8ebd
Marmare314 <49279081+Marmare314@users.noreply.github.com>
2026-05-20 13:11:38 +0200

    when changing animation speed adjust start time to avoid jumps Also use `Client()->GlobalTime()` instead of `time_get()`.
    (cherry picked from commit 8948715713cdd822749ea9ecde69b886066743c6)

--- src/game/editor/editor.cpp
+++ src/game/editor/editor.cpp
@@ -812,7 +812,7 @@ void CEditor::DoToolbarLayers(CUIRect ToolBar)
 		if(DoButton_FontIcon(&s_AnimateButton, FONT_ICON_CIRCLE_PLAY, m_Animate, &Button, BUTTONFLAG_LEFT, "[Ctrl+M] 切换动画.", IGraphics::CORNER_L) ||
 			(m_Dialog == DIALOG_NONE && CLineInput::GetActiveInput() == nullptr && Input()->KeyPress(KEY_M) && ModPressed))
 		{
-			m_AnimateStart = time_get();
+			m_AnimateStart = Client()->GlobalTime();
 			m_Animate = !m_Animate;
 		}
 
@@ -8541,7 +8541,7 @@ void CEditor::OnRender()
 		m_ShowMousePointer = false;
 
 	if(m_Animate)
-		m_AnimateTime = (time_get() - m_AnimateStart) / (float)time_freq();
+		m_AnimateTime = Client()->GlobalTime() - m_AnimateStart;
 	else
 		m_AnimateTime = 0;
 
