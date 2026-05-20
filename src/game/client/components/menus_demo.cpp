35468f62edd98c83a09c707234b976b73827a4ae
Robert Müller <robytemueller@gmail.com>
2026-05-20 22:39:46 +0200

    Fix demo browser UI stuck when refreshing inside deleted folder
    When refreshing the demo browser after the current folder has been deleted externally or when access to the folder is otherwise not possible, the demo browser did not show the `../` item to navigate back to the parent folder, so it was not possible to use the demo browser until restarting the client.
    
    (cherry picked from commit bcd2631e8b6ae95cd37251bd22a7e48f7383d507)

--- src/game/client/components/menus_demo.cpp
+++ src/game/client/components/menus_demo.cpp
@@ -1483,6 +1483,25 @@ void CMenus::DemolistPopulate()
 		m_DemoPopulateStartTime = time_get_nanoseconds();
 		Storage()->ListDirectory(m_DemolistStorageType, m_aCurrentDemoFolder, DemolistFetchCallback, this);
 
+		// Make sure there is a demo item to navigate back to the parent folder, if the folder contents could not be enumerated.
+		if(m_vDemos.empty())
+		{
+			CDemoItem Item;
+			str_copy(Item.m_aFilename, "..");
+			str_copy(Item.m_aName, "../");
+			Item.m_Date = 0;
+			Item.m_DateLoaded = true;
+			Item.m_DateValid = false;
+			Item.m_Size = 0;
+			Item.m_SizeLoaded = true;
+			Item.m_InfosLoaded = false;
+			Item.m_Valid = false;
+			Item.m_IsDir = true;
+			Item.m_IsLink = false;
+			Item.m_StorageType = m_DemolistStorageType;
+			m_vDemos.push_back(Item);
+		}
+
 		std::stable_sort(m_vDemos.begin(), m_vDemos.end());
 	}
 	ResetDemoBrowserMetadataProgress();
