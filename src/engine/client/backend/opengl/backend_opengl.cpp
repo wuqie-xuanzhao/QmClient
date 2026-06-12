9a23f59190f307f37b5ef8813300fe1b9e43fa75
Robert Müller <robytemueller@gmail.com>
2026-06-12 21:24:14 +0200

    Cleanup OpenGL driver version string parsing
    Fix last version number component not being parsed if the driver version string ends immediately after a digit. Version strings in practice are usually followed by space and the driver name and version.
    
    Parse at most 9 digits per version number component to avoid overflow.
    
    Remove unnecessary `CurNumberStrLen > 0` check which was always `true`.
    
    Move `TotalNumbersPassed < 3` check after incrementing the `TotalNumbersPassed` variable. Use `std::size(aNumbers)` instead of `3`.

--- src/engine/client/backend/opengl/backend_opengl.cpp
+++ src/engine/client/backend/opengl/backend_opengl.cpp
@@ -167,18 +167,16 @@ void CCommandProcessorFragment_OpenGL::SetState(const CCommandBuffer::SState &St
 
 static void ParseVersionString(EBackendType BackendType, const char *pStr, int &VersionMajor, int &VersionMinor, int &VersionPatch)
 {
-	if(pStr)
+	// If the backend is GLES, the version string starts with `OpenGL ES ` or `OpenGL ES-CM ` for older contexts, rest is the same.
+	if(BackendType == BACKEND_TYPE_OPENGL_ES)
 	{
-		// if backend is GLES, it starts with "OpenGL ES " or OpenGL ES-CM for older contexts, rest is the same
-		if(BackendType == BACKEND_TYPE_OPENGL_ES)
+		const char *pSkippedPrefix;
+		if((pSkippedPrefix = str_startswith(pStr, "OpenGL ES ")) != nullptr ||
+			(pSkippedPrefix = str_startswith(pStr, "OpenGL ES-CM ")) != nullptr)
 		{
-			int StrLenGLES = str_length("OpenGL ES ");
-			int StrLenGLESCM = str_length("OpenGL ES-CM ");
-			if(str_comp_num(pStr, "OpenGL ES ", StrLenGLES) == 0)
-				pStr += StrLenGLES;
-			else if(str_comp_num(pStr, "OpenGL ES-CM ", StrLenGLESCM) == 0)
-				pStr += StrLenGLESCM;
+			pStr = pSkippedPrefix;
 		}
+	}
 
 	char aCurNumberStr[10];
 	size_t CurNumberStrLen = 0;
@@ -195,7 +193,7 @@ static void ParseVersionString(EBackendType BackendType, const char *pStr, int &
 				Error = true;
 				break;
 			}
-			aCurNumberStr[CurNumberStrLen++] = (char)*pStr;
+			aCurNumberStr[CurNumberStrLen++] = *pStr;
 			LastWasNumber = true;
 		}
 		else if(LastWasNumber && (*pStr == '.' || *pStr == ' ' || *pStr == '\0'))
