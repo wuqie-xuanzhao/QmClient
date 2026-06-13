3f6d7fc7c417f72311e2a6c185ab6a03abc31814
heinrich5991 <heinrich5991@gmail.com>
2026-06-13 12:52:40 +0200

    Resolve `TODO(cxx)` comments
    The new version of cxx allows us to drop some backward compatibility
    hacks.
    
    (cherry picked from commit 2e0ab8a06d8c325fa24e76b6c8591f9f356e4ef9)

--- src/game/client/sixup_translate_snapshot.cpp
+++ src/game/client/sixup_translate_snapshot.cpp
@@ -8,7 +8,7 @@
 
 int CGameClient::TranslateSnap(CSnapshotBuffer *pSnapDstSix, CSnapshot *pSnapSrcSeven, int Conn, bool Dummy)
 {
-	rust::Box<CSnapshotBuilder> pBuilder = CSnapshotBuilder_New();
+	rust::Box<CSnapshotBuilder> pBuilder = CSnapshotBuilder::New();
 	pBuilder->Init(false);
 
 	float LocalTime = Client()->LocalTime();
@@ -440,7 +440,7 @@ int CGameClient::TranslateSnap(CSnapshotBuffer *pSnapDstSix, CSnapshot *pSnapSrc
 
 int CGameClient::OnDemoRecSnap7(CSnapshot *pFrom, CSnapshotBuffer *pTo, int Conn)
 {
-	rust::Box<CSnapshotBuilder> pBuilder = CSnapshotBuilder_New();
+	rust::Box<CSnapshotBuilder> pBuilder = CSnapshotBuilder::New();
 	pBuilder->Init(false);
 
 	{
