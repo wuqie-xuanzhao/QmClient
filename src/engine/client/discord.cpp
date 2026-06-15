f47a8551ee3a79512cb9eab6db6c4fc31caee494
Robert Müller <robytemueller@gmail.com>
2026-06-15 22:31:08 +0200

    Check clang-tidy with all optional features enabled
    (cherry picked from commit ad970b38d320f0f66ef8acf547f37335010e1dbc)

--- src/engine/client/discord.cpp
+++ src/engine/client/discord.cpp
@@ -10,7 +10,7 @@ typedef enum EDiscordResult(DISCORD_API *FDiscordCreate)(DiscordVersion, struct 
 
 #if defined(CONF_DISCORD_DYNAMIC)
 #include <dlfcn.h>
-FDiscordCreate GetDiscordCreate()
+static FDiscordCreate GetDiscordCreate()
 {
 	void *pSdk = dlopen("discord_game_sdk.so", RTLD_NOW);
 	if(!pSdk)
@@ -20,7 +20,7 @@ FDiscordCreate GetDiscordCreate()
 	return (FDiscordCreate)dlsym(pSdk, "DiscordCreate");
 }
 #else
-FDiscordCreate GetDiscordCreate()
+static FDiscordCreate GetDiscordCreate()
 {
 	return DiscordCreate;
 }
@@ -46,13 +46,14 @@ public:
 		m_Enabled = false;
 		return InitDiscord();
 	}
+
 	bool InitDiscord()
 	{
 		if(m_pCore)
 		{
 			m_pCore->destroy(m_pCore);
-			m_pCore = 0;
-			m_pActivityManager = 0;
+			m_pCore = nullptr;
+			m_pActivityManager = nullptr;
 		}
 		if(!m_Enabled)
 			return false;
@@ -60,7 +61,7 @@ public:
 		mem_zero(&m_ActivityEvents, sizeof(m_ActivityEvents));
 
 		m_ActivityEvents.on_activity_join = &CDiscord::OnActivityJoin;
-		m_pActivityManager = 0;
+		m_pActivityManager = nullptr;
 
 		DiscordCreateParams Params;
 		DiscordCreateParamsSetDefault(&Params);
@@ -106,7 +107,7 @@ public:
 				m_UpdateActivity = false;
 				m_LastActivityUpdate = time_get();
 
-				m_pActivityManager->update_activity(m_pActivityManager, &m_Activity, 0, 0);
+				m_pActivityManager->update_activity(m_pActivityManager, &m_Activity, nullptr, nullptr);
 			}
 
 			m_pCore->run_callbacks(m_pCore);
@@ -210,8 +211,8 @@ public:
 	static void DISCORD_CALLBACK OnActivityJoin(void *pEventData, const char *pSecret)
 	{
 		CDiscord *pSelf = static_cast<CDiscord *>(pEventData);
-		IClient *m_pClient = pSelf->Kernel()->RequestInterface<IClient>();
-		m_pClient->Connect(pSecret);
+		IClient *pClient = pSelf->Kernel()->RequestInterface<IClient>();
+		pClient->Connect(pSecret);
 	}
 
 	~CDiscord()
@@ -226,13 +227,13 @@ static IDiscord *CreateDiscordImpl()
 	FDiscordCreate pfnDiscordCreate = GetDiscordCreate();
 	if(!pfnDiscordCreate)
 	{
-		return 0;
+		return nullptr;
 	}
 	CDiscord *pDiscord = new CDiscord();
 	if(pDiscord->Init(pfnDiscordCreate))
 	{
 		delete pDiscord;
-		return 0;
+		return nullptr;
 	}
 	return pDiscord;
 }
--- src/engine/server/antibot.cpp
+++ src/engine/server/antibot.cpp
@@ -12,7 +12,7 @@ class IEngineAntibot;
 
 #ifdef CONF_ANTIBOT
 CAntibot::CAntibot() :
-	m_pServer(0), m_pConsole(0), m_pGameServer(0), m_Initialized(false)
+	m_pServer(nullptr), m_pConsole(nullptr), m_pGameServer(nullptr), m_Initialized(false)
 {
 }
 CAntibot::~CAntibot()
@@ -86,7 +86,7 @@ void CAntibot::RoundStart(IGameServer *pGameServer)
 {
 	m_pGameServer = pGameServer;
 	mem_zero(&m_RoundData, sizeof(m_RoundData));
-	m_RoundData.m_Map.m_pTiles = 0;
+	m_RoundData.m_Map.m_pTiles = nullptr;
 	AntibotRoundStart(&m_RoundData);
 	Update();
 }
@@ -95,7 +95,7 @@ void CAntibot::RoundEnd()
 	// Let the external module clean up first
 	AntibotRoundEnd();
 
-	m_pGameServer = 0;
+	m_pGameServer = nullptr;
 	free(m_RoundData.m_Map.m_pTiles);
 }
 void CAntibot::ConsoleCommand(const char *pCommand)
