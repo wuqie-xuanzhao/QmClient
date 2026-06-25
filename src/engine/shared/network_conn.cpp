a3311f7a71b1f06e905f7195aa150bc86e22483e
Robert Müller <robytemueller@gmail.com>
2026-06-25 21:35:54 +0200

    Ensure network connection error message is valid UTF-8
    (cherry picked from commit 0837d348f43c23ebd978f19211d8bbe9fb5174b0)

--- src/engine/shared/network_conn.cpp
+++ src/engine/shared/network_conn.cpp
@@ -483,6 +483,10 @@ int CNetConnection::Feed(CNetPacketConstruct *pPacket, NETADDR *pAddr, SECURITY_
 					// make sure to sanitize the error string from the other party
 					str_copy(aStr, (char *)&pPacket->m_aChunkData[1], minimum(pPacket->m_DataSize, (int)sizeof(aStr)));
 					str_sanitize_cc(aStr);
+					if(!str_utf8_check(aStr))
+					{
+						str_copy(aStr, "(Invalid error message)");
+					}
 				}
 
 				if(!m_BlockCloseMsg)
