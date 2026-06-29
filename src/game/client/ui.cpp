ddnet/ddnet

Add back button

src/game/client/ui.cpp
@@ -1662,6 +1662,104 @@ void CUi::RenderProgressSpinner(vec2 Center, float OuterRadius, const SProgressS
 	Graphics()->QuadsEnd();
 }
 
+void CUi::DoBackButton()
+{
+	if(!g_Config.m_ClBackButton)
+		return;
+
+	MapScreen();
+	const CUIRect *pScreen = Screen();
+	const float Size = pScreen->h * 0.1f;
+	constexpr float PositionScale = 1000000.0f;
+	const auto ClampPos = [&](vec2 Pos) {
+		Pos.x = std::clamp(Pos.x, 0.0f, pScreen->w - Size);
+		Pos.y = std::clamp(Pos.y, 0.0f, pScreen->h - Size);
+		return Pos;
+	};
+
+	vec2 ButtonPos = ClampPos({g_Config.m_ClBackButtonX / PositionScale * pScreen->w, g_Config.m_ClBackButtonY / PositionScale * pScreen->h});
+	CUIRect ButtonRect{ButtonPos.x, ButtonPos.y, Size, Size};
+
+	bool Clicked = false;
+	bool Abrupted = false;
+	const int Result = DoDraggableButtonLogic(&m_BackButtonId, 0, &ButtonRect, &Clicked, &Abrupted);
+
+	// Detect the press transition. DoDraggableButtonLogic sets the active item on the
+	// press frame but returns 0 there, so check CheckActiveItem to catch it.
+	if(m_BackButtonOp == EBackButtonOp::NONE && CheckActiveItem(&m_BackButtonId))
+	{
+		m_BackButtonInitialMouse = MousePos();
+		m_BackButtonDragOffset = ButtonPos - MousePos();
+		m_BackButtonOp = EBackButtonOp::CLICKED;
+		if(m_pfnOnBackButtonPressed)
+			m_pfnOnBackButtonPressed();
+	}
+
+	if(m_BackButtonOp == EBackButtonOp::CLICKED && length(MousePos() - m_BackButtonInitialMouse) > 5.0f)
+	{
+		m_BackButtonOp = EBackButtonOp::DRAGGING;
+	}
+
+	if(m_BackButtonOp == EBackButtonOp::DRAGGING)
+	{
+		ButtonPos = ClampPos(MousePos() + m_BackButtonDragOffset);
+		g_Config.m_ClBackButtonX = round_to_int(ButtonPos.x / pScreen->w * PositionScale);
+		g_Config.m_ClBackButtonY = round_to_int(ButtonPos.y / pScreen->h * PositionScale);
+		ButtonRect.x = ButtonPos.x;
+		ButtonRect.y = ButtonPos.y;
+	}
+
+	if(Result && Clicked)
+	{
+		if(m_BackButtonOp == EBackButtonOp::CLICKED && m_pfnDispatchInput)
+		{
+			IInput::CEvent Event;
+			Event.m_Key = KEY_ESCAPE;
+			Event.m_InputCount = 0;
+			Event.m_aText[0] = '\0';
+			Event.m_Flags = IInput::FLAG_PRESS;
+			m_pfnDispatchInput(Event);
+			Event.m_Flags = IInput::FLAG_RELEASE;
+			m_pfnDispatchInput(Event);
+		}
+		m_BackButtonOp = EBackButtonOp::NONE;
+	}
+	else if(Result && Abrupted)
+	{
+		m_BackButtonOp = EBackButtonOp::NONE;
+	}
+
+	m_BackButtonRect = ButtonRect;
+}
+
+void CUi::RenderBackButton()
+{
+	if(!g_Config.m_ClBackButton)
+		return;
+
+	MapScreen();
+
+	// Override hot/active claims made by UI rendered between DoBackButton and RenderBackButton.
+	if(m_BackButtonOp != EBackButtonOp::NONE)
+		SetActiveItem(&m_BackButtonId);
+	else if(MouseHovered(&m_BackButtonRect) && !MouseButton(0) && !MouseButton(1) && !MouseButton(2))
+		SetHotItem(&m_BackButtonId);
+
+	const bool Pressed = m_BackButtonOp != EBackButtonOp::NONE;
+	const bool Hovered = !Pressed && HotItem() == &m_BackButtonId;
+	const float Alpha = Pressed ? 0.9f : (Hovered ? 0.35f : 0.5f);
+	const ColorRGBA BackgroundColor{0.0f, 0.0f, 0.0f, Alpha};
+	m_BackButtonRect.Draw(BackgroundColor, IGraphics::CORNER_ALL, 12.0f);
+
+	TextRender()->SetFontPreset(EFontPreset::ICON_FONT);
+	TextRender()->SetRenderFlags(ETextRenderFlags::TEXT_RENDER_FLAG_ONLY_ADVANCE_WIDTH |
+				     ETextRenderFlags::TEXT_RENDER_FLAG_NO_X_BEARING |
+				     ETextRenderFlags::TEXT_RENDER_FLAG_NO_Y_BEARING);
+	DoLabel(&m_BackButtonRect, FontIcon::CHEVRON_LEFT, m_BackButtonRect.w * 0.5f, TEXTALIGN_MC);
+	TextRender()->SetRenderFlags(0);
+	TextRender()->SetFontPreset(EFontPreset::DEFAULT_FONT);
+}
+
 void CUi::DoPopupMenu(const SPopupMenuId *pId, float X, float Y, float Width, float Height, void *pContext, FPopupMenuFunction pfnFunc, const SPopupMenuProperties &Props)
 {
 	constexpr float Margin = SPopupMenu::POPUP_BORDER + SPopupMenu::POPUP_MARGIN;

src/game/client/ui.h
@@ -413,6 +413,21 @@ private:
 
 	unsigned m_HotkeysPressed = 0;
 
+	enum class EBackButtonOp
+	{
+		NONE,
+		CLICKED,
+		DRAGGING,
+	};
+	EBackButtonOp m_BackButtonOp = EBackButtonOp::NONE;
+	vec2 m_BackButtonDragOffset = vec2(0.0f, 0.0f);
+	vec2 m_BackButtonInitialMouse = vec2(0.0f, 0.0f);
+	CUIRect m_BackButtonRect = {0.0f, 0.0f, 0.0f, 0.0f};
+	char m_BackButtonId = 0;
+
+	std::function<void(const IInput::CEvent &Event)> m_pfnDispatchInput;
+	std::function<void()> m_pfnOnBackButtonPressed;
+
 	CUIRect m_Screen;
 
 	std::vector<CUIRect> m_vClips;
@@ -676,6 +691,13 @@ public:
 
 	// progress spinner
 	void RenderProgressSpinner(vec2 Center, float OuterRadius, const SProgressSpinnerProperties &Props = {}) const;
+
+	// virtual back button
+	void DoBackButton();
+	void RenderBackButton();
+	void SetDispatchInputCallback(std::function<void(const IInput::CEvent &Event)> pfnCallback) { m_pfnDispatchInput = std::move(pfnCallback); }
+	// Fired the moment the back button transitions to active (mouse-down inside it).
+	void SetOnBackButtonPressedCallback(std::function<void()> pfnCallback) { m_pfnOnBackButtonPressed = std::move(pfnCallback); }
 
 	// popup menu
 	void DoPopupMenu(const SPopupMenuId *pId, float X, float Y, float Width, float Height, void *pContext, FPopupMenuFunction pfnFunc, const SPopupMenuProperties &Props = {});

wuqie-xuanzhao/QmClient

(cherry picked from commit 775365126dd8b88a10ef63e8d609c9afc7cde36d)

src/game/client/ui.cpp
@@ -1889,6 +1889,103 @@ void CUi::RenderProgressSpinner(vec2 Center, float OuterRadius, const SProgressS
 	Graphics()->QuadsEnd();
 }
 
+void CUi::DoBackButton()
+{
+	if(!g_Config.m_ClBackButton)
+		return;
+
+	MapScreen();
+	const CUIRect *pScreen = Screen();
+	const float Size = pScreen->h * 0.1f;
+	constexpr float PositionScale = 1000000.0f;
+	const auto ClampPos = [&](vec2 Pos) {
+		Pos.x = std::clamp(Pos.x, 0.0f, pScreen->w - Size);
+		Pos.y = std::clamp(Pos.y, 0.0f, pScreen->h - Size);
+		return Pos;
+	};
+
+	vec2 ButtonPos = ClampPos({g_Config.m_ClBackButtonX / PositionScale * pScreen->w, g_Config.m_ClBackButtonY / PositionScale * pScreen->h});
+	CUIRect ButtonRect{ButtonPos.x, ButtonPos.y, Size, Size};
+
+	bool Clicked = false;
+	bool Abrupted = false;
+	const int Result = DoDraggableButtonLogic(&m_BackButtonId, 0, &ButtonRect, &Clicked, &Abrupted);
+
+	// Detect the press transition. DoDraggableButtonLogic sets the active item on the
+	// press frame but returns 0 there, so check CheckActiveItem to catch it.
+	if(m_BackButtonOp == EBackButtonOp::NONE && CheckActiveItem(&m_BackButtonId))
+	{
+		m_BackButtonInitialMouse = MousePos();
+		m_BackButtonDragOffset = ButtonPos - MousePos();
+		m_BackButtonOp = EBackButtonOp::CLICKED;
+		if(m_OnBackButtonPressedFunction)
+			m_OnBackButtonPressedFunction();
+	}
+
+	if(m_BackButtonOp == EBackButtonOp::CLICKED && length(MousePos() - m_BackButtonInitialMouse) > 5.0f)
+	{
+		m_BackButtonOp = EBackButtonOp::DRAGGING;
+	}
+
+	if(m_BackButtonOp == EBackButtonOp::DRAGGING)
+	{
+		ButtonPos = ClampPos(MousePos() + m_BackButtonDragOffset);
+		g_Config.m_ClBackButtonX = round_to_int(ButtonPos.x / pScreen->w * PositionScale);
+		g_Config.m_ClBackButtonY = round_to_int(ButtonPos.y / pScreen->h * PositionScale);
+		ButtonRect.x = ButtonPos.x;
+		ButtonRect.y = ButtonPos.y;
+	}
+
+	if(Result && Clicked)
+	{
+		if(m_BackButtonOp == EBackButtonOp::CLICKED && m_DispatchInputFunction)
+		{
+			IInput::CEvent Event;
+			Event.m_Key = KEY_ESCAPE;
+			Event.m_InputCount = 0;
+			Event.m_aText[0] = '\0';
+			Event.m_Flags = IInput::FLAG_PRESS;
+			m_DispatchInputFunction(Event);
+			Event.m_Flags = IInput::FLAG_RELEASE;
+			m_DispatchInputFunction(Event);
+		}
+		m_BackButtonOp = EBackButtonOp::NONE;
+	}
+	else if(Result && Abrupted)
+	{
+		m_BackButtonOp = EBackButtonOp::NONE;
+	}
+
+	m_BackButtonRect = ButtonRect;
+}
+
+void CUi::RenderBackButton()
+{
+	if(!g_Config.m_ClBackButton)
+		return;
+
+	MapScreen();
+
+	// Override hot/active claims made by UI rendered between DoBackButton and RenderBackButton.
+	if(m_BackButtonOp != EBackButtonOp::NONE)
+		SetActiveItem(&m_BackButtonId);
+	else if(MouseHovered(&m_BackButtonRect) && !MouseButton(0) && !MouseButton(1) && !MouseButton(2))
+		SetHotItem(&m_BackButtonId);
+
+	const bool Pressed = m_BackButtonOp != EBackButtonOp::NONE;
+	const bool Hovered = !Pressed && HotItem() == &m_BackButtonId;
+	const float Alpha = Pressed ? 0.9f : (Hovered ? 0.35f : 0.5f);
+	m_BackButtonRect.Draw({0.0f, 0.0f, 0.0f, Alpha}, IGraphics::CORNER_ALL, 12.0f);
+
+	TextRender()->SetFontPreset(EFontPreset::ICON_FONT);
+	TextRender()->SetRenderFlags(ETextRenderFlags::TEXT_RENDER_FLAG_ONLY_ADVANCE_WIDTH |
+				     ETextRenderFlags::TEXT_RENDER_FLAG_NO_X_BEARING |
+				     ETextRenderFlags::TEXT_RENDER_FLAG_NO_Y_BEARING);
+	DoLabel(&m_BackButtonRect, FontIcon::CHEVRON_LEFT, m_BackButtonRect.w * 0.5f, TEXTALIGN_MC);
+	TextRender()->SetRenderFlags(0);
+	TextRender()->SetFontPreset(EFontPreset::DEFAULT_FONT);
+}
+
 void CUi::DoPopupMenu(const SPopupMenuId *pId, float X, float Y, float Width, float Height, void *pContext, FPopupMenuFunction pfnFunc, const SPopupMenuProperties &Props)
 {
 	constexpr float Margin = SPopupMenu::POPUP_BORDER + SPopupMenu::POPUP_MARGIN;

src/game/client/ui.h
@@ -500,6 +500,21 @@ private:
 
 	unsigned m_HotkeysPressed = 0;
 
+	enum class EBackButtonOp
+	{
+		NONE,
+		CLICKED,
+		DRAGGING,
+	};
+	EBackButtonOp m_BackButtonOp = EBackButtonOp::NONE;
+	vec2 m_BackButtonDragOffset = vec2(0.0f, 0.0f);
+	vec2 m_BackButtonInitialMouse = vec2(0.0f, 0.0f);
+	CUIRect m_BackButtonRect = {0.0f, 0.0f, 0.0f, 0.0f};
+	const char m_BackButtonId = 0;
+
+	std::function<void(const IInput::CEvent &Event)> m_DispatchInputFunction;
+	std::function<void()> m_OnBackButtonPressedFunction;
+
 	CUIRect m_Screen;
 
 	std::vector<CUIRect> m_vClips;
@@ -800,6 +815,13 @@ public:
 
 	// progress spinner
 	void RenderProgressSpinner(vec2 Center, float OuterRadius, const SProgressSpinnerProperties &Props = {}) const;
+
+	// virtual back button
+	void DoBackButton();
+	void RenderBackButton();
+	void SetDispatchInputCallback(std::function<void(const IInput::CEvent &Event)> pfnCallback) { m_DispatchInputFunction = std::move(pfnCallback); }
+	// Fired the moment the back button transitions to active (mouse-down inside it).
+	void SetOnBackButtonPressedCallback(std::function<void()> pfnCallback) { m_OnBackButtonPressedFunction = std::move(pfnCallback); }
 
 	// popup menu
 	void DoPopupMenu(const SPopupMenuId *pId, float X, float Y, float Width, float Height, void *pContext, FPopupMenuFunction pfnFunc, const SPopupMenuProperties &Props = {});