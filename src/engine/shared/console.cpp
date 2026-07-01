d2c28eba31a33e3c69a9b84a6094eb97da0a3566
“wuqie-xuanzhao” <yuanliyinling@gmail.com>
2026-07-01 23:57:53 +0800

    fix(sync): 清理命令列表合并残留

--- src/engine/shared/console.cpp
+++ src/engine/shared/console.cpp
@@ -854,7 +854,7 @@ void CConsole::ConCommandStatus(IResult *pResult, void *pUser)
 void CConsole::ConUserCommandStatus(IResult *pResult, void *pUser)
 {
 	CConsole *pConsole = static_cast<CConsole *>(pUser);
-<<	pConsole->Print(OUTPUT_LEVEL_STANDARD, "chatresp", "可用聊天命令列表：");
+	pConsole->Print(OUTPUT_LEVEL_STANDARD, "chatresp", "可用聊天命令列表：");
 	//List all commands which are accessible for users
 	pConsole->PrintCommandList(EAccessLevel::USER, CMDFLAG_PRACTICE);
 }
