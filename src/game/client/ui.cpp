215c578128c42a6a84db66bc55e075d367f9737d
Robert Müller <robytemueller@gmail.com>
2026-08-16 23:41:25 +0200

    Fix assertion crash when using virtual back button
    The back button operation was not properly reset when using the `CUi::DoDraggableButtonLogic` function, so the client was crashing with the `m_ActiveDraggableButtonLogicButton invalid` assertion when pressing and releasing the right/middle mouse button immediately after hovering and leaving the back button with the mouse.
    
    (cherry picked from commit 624f58e47781c4fee830a1a2b8892936b31f1e40)

--- src/game/client/ui.cpp
+++ src/game/client/ui.cpp
@@ -2557,9 +2557,9 @@ void CUi::DoBackButton()
 		ButtonRect.y = ButtonPos.y;
 	}
 
-	if(Result && Clicked)
+	if(Clicked || Abrupted)
 	{
-		if(m_BackButtonOp == EBackButtonOp::CLICKED && m_DispatchInputFunction)
+		if(Result && Clicked && m_BackButtonOp == EBackButtonOp::CLICKED && m_DispatchInputFunction)
 		{
 			IInput::CEvent Event;
 			Event.m_Key = KEY_ESCAPE;
@@ -2570,10 +2570,6 @@ void CUi::DoBackButton()
 			Event.m_Flags = IInput::FLAG_RELEASE;
 			m_DispatchInputFunction(Event);
 		}
-		m_BackButtonOp = EBackButtonOp::NONE;
-	}
-	else if(Result && Abrupted)
-	{
 		m_BackButtonOp = EBackButtonOp::NONE;
 	}
 
